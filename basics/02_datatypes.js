4a84c2d1ca3cd382f6275192f24f6a272c12e87c
bhavya2701 <bhavya.pj27@gmail.com>
2024-09-19 18:00:09 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+
+//alert(3+3)// we are using nodejs ,not browser 
+console.log(3+3)
+console.log("bhavya") //code readability
+let name ="bhavya"
+let age =18 
+let isloggedin =false 
+let state =null
+
+
+//number ==>2 to power 53 
+//bigint agar number bohot zyada hai 
+//string double or single quotes 
+//boolean ==>true or false
+//null ==> stand alone value representation of empty value
+//undefined ==> variable declared but value not given 
+//symbol ==> uniquemess 
+console.log(typeof "bhavya")
+console.log(typeof null)
+
