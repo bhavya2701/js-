c82f0120e8f076b687e8ebdf8cfcb5e1a4c13a9b
bhavya2701 <bhavya.pj27@gmail.com>
2024-09-19 18:00:33 +0000

    conversions

---
+++ basics/03_conversionsOperations.js
@@ -0,0 +1,18 @@
+let score ='44'
+console.log(typeof score)
+let valueinnummber = Number(score)
+console.log(typeof valueinnummber)
+//"33 " ==>33
+//"33abc" ==>NaN
+//TRUE ==>1 FALSE ==>0
+let isloggedin = "BHAVYA"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+//"" ==>FALSE 
+// "BHAVYA" ==TRUE 
+
+let somenumber=33
+let stringnumber =String(somenumber)
+console.log(stringnumber);
+console.log( typeof stringnumber);
